e053eb86381a6dd99427b40854abd92914a999a2
Rinaldiaulia10 <rinaldiaulia010@gmail.com>
2025-06-19 16:41:07 +0700

    Sintaksis Perulangan

---
+++ Dasar_sintaksis_perulangan.py
@@ -0,0 +1,13 @@
+"""
+Program berulang membaca buku
+"""
+Jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+
+Jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
+
+for jumlah_buku_yang_dibaca in range(0, Jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_yang_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}")
